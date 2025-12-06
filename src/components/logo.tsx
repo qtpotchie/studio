b609a00d8750a2580670ea2a071d35a5fd996459
Potchie qt <qtpotchie@gmail.com>
2025-12-06 05:15:16 +0000

    add a border to the icon (_for element svg_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,6 +10,7 @@ const Logo = ({ className }: { className?: string }) => (
     xmlns="http://www.w3.org/2000/svg"
     aria-label="TechTermz Logo"
   >
+    <rect x="2" y="2" width="20" height="20" rx="2" stroke="hsl(var(--foreground))" strokeWidth="1.5" />
     <path
       d="M4 19.5C4 18.5 4 17.5 4 16C4 12 7.5 11 12 11C16.5 11 20 12 20 16C20 17.5 20 18.5 20 19.5M4 19.5C4 20.5 4 21.5 4 22H20V19.5M4 19.5V4H20V19.5"
       stroke="hsl(var(--foreground))"
