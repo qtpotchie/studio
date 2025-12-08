64a8287ec2bf57ba7616dde78fff8efdeb6095ef
Potchie qt <qtpotchie@gmail.com>
2025-12-08 03:51:58 +0000

    remove (_for element <LinkComponent>_)

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import Logo from './logo';
 import { Button } from './ui/button';
-import { Bookmark, History, Menu, HomeIcon, CalendarDays, BookOpen, Mic } from 'lucide-react';
+import { Menu, Mic } from 'lucide-react';
 import { useSearch } from '@/hooks/use-search';
 import { useVoiceSearch } from '@/context/voice-search-context';
 import { useMobileSidebar } from '@/hooks/use-mobile-sidebar';
@@ -17,12 +17,6 @@ export default function Header() {
     <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
         <div className="mr-4 flex">
-          <Button variant="ghost" size="icon" onClick={onOpen} className="md:hidden">
-            <Menu className="h-5 w-5" />
-            <span className="sr-only">Open menu</span>
-          </Button>
-        </div>
-        <div className="mr-4 flex items-center">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <Logo />
             <span className="font-bold">TechTermz</span>
@@ -41,11 +35,6 @@ export default function Header() {
              <Button variant="default" size="icon" onClick={() => setVoiceOpen(true)}>
               <Mic className="h-5 w-5" />
               <span className="sr-only">Voice Search</span>
-            </Button>
-            <Button asChild variant="ghost" size="icon">
-              <Link href="/bookmarks" aria-label="View Bookmarks">
-                <Bookmark className="h-5 w-5" />
-              </Link>
             </Button>
           </nav>
            <Button variant="ghost" size="icon" onClick={onOpen}>
