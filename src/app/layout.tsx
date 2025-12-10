d05b29e0b4e97a5e4c878d60697c64c25b645b5f
Potchie qt <qtpotchie@gmail.com>
2025-12-10 16:58:53 +0000

    hindi tayo mag kaigi ay gawin mo nalang white tas iadjust mo lahat nung

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${inter.variable} ${fontHeadline.variable} dark`} suppressHydrationWarning>
+    <html lang="en" className={`${inter.variable} ${fontHeadline.variable}`} suppressHydrationWarning>
       <head>
       </head>
       <body className="font-sans antialiased">
