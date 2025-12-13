14a6541a913d7ddc4c3ae91b33ecdd8dd77d6aa3
Potchie qt <qtpotchie@gmail.com>
2025-12-13 01:21:33 +0000

    wala man akong na download na ngrok file

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -21,6 +22,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
