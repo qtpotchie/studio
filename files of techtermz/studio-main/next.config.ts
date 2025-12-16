09cf613de41e1b4fe18d84e9462ecb3a9ee9679a
qtpotchie <qtpotchie@gmail.com>
2025-12-17 00:19:10 +0800

    Add files via upload

--- files of techtermz/studio-main/next.config.ts
+++ files of techtermz/studio-main/next.config.ts
@@ -14,6 +14,7 @@ const withPWA = require("@ducanh2912/next-pwa").default({
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
